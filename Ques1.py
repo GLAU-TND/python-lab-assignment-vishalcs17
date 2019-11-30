f1e3dbb52df3fd0d7afac158c24668bb0a71e0a9
Vishal Rawal <46665789+vishalcs17@users.noreply.github.com>
2019-11-30 16:01:05 +0530

    Add files via upload

---
+++ Ques1.py
@@ -0,0 +1,12 @@
+class MyException(Exception):
+    def __init__(self,v=''):
+        self.v=v
+    def __str__(self):
+        return str(self)
+def Add(a,b):
+    if a+b>=150:
+        return a+b
+    else:
+        raise MyException('Result is not greater than 150')
+re = Add(30,40)
+print('Result is',re)
